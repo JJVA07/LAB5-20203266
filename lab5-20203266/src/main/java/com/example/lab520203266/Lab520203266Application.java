7652fb6719673c42cb0236d729e837bfabebc8d0
JJVA07 <a20203266@pucp.edu.pe>
2025-05-15 20:25:04 -0500

    Primer commit

---
+++ lab5-20203266/src/main/java/com/example/lab520203266/Lab520203266Application.java
@@ -0,0 +1,13 @@
+package com.example.lab520203266;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab520203266Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab520203266Application.class, args);
+    }
+
+}
---
+++ lab5-20203266/src/test/java/com/example/lab520203266/Lab520203266ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab520203266;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab520203266ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
